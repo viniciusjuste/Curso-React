b2824ce53c1f5879b4c45822fcbc5fee63ea03dd
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-09-18 21:10:56 -0300

    Entendendo a estrutura básica de um App React

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprender React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return(
+    <>
+    <p>CFB Cursos</p>
+    <p>Curso de React</p>
+    </>
+  )
 }
-
-export default App;
