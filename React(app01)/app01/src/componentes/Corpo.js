a5afe7deb7e7b26edfe5552e3c46f82c045bdb09
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-09-25 20:28:48 -0300

    Enviar funções para os componentes

--- React(app01)/app01/src/componentes/Corpo.js
+++ React(app01)/app01/src/componentes/Corpo.js
@@ -3,7 +3,14 @@ import Dados from "./Dados";
 
 export default function Corpo() {
 
-    const cnl = 'CFB Cursos';
+    const cnl = () => {
+        return 'CFB Cursos';
+    }
+
+    const somar = (v1, v2) => {
+        return v1 + v2
+    }
+
     const yt = 'youtube.com/cfbcursos';
     const crs = 'React'
     return (
@@ -11,7 +18,7 @@ export default function Corpo() {
             <h2>Curso de React</h2>
             <p>Se inscreva em nosso canal</p>
             <p>Ative o sininho e clique no joinha</p>
-            <Dados canal={cnl} youtube={yt} curso={crs} />
+            <Dados canal={cnl} youtube={yt} curso={crs} soma={somar} />
         </section>
     )
 }
--- React(app01)/app01/src/componentes/Dados.js
+++ React(app01)/app01/src/componentes/Dados.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export default function Dados(props) {
-
+    const n1 = 20;
+    const n2 = 30;
     return (
         <section>
             <p>Canal: {props.canal}</p>
             <p>YouTube: {props.youtube}</p>
             <p>Curso: {props.curso}</p>
+            <p>{'O valor da soma de ' + n1 + ' e ' + n2 + ' é igual a ' + props.soma(n1, n2)}</p>
         </section>
     )
 }
