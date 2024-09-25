11fb1946b79995434817eaa2bc79f1e32f2ef3d0
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-09-25 20:58:47 -0300

    sando CSS em React, formas de usar CSS em React

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import './App.css' // É assim que se importa o arquivo CSS
 import Header from "./componentes/Header";
 import Corpo from "./componentes/Corpo";
 import Footer, { getDate } from "./componentes/Footer"; // Importei o Footer e uma função que retorna o ano atual
 
 export default function App() {
 
+  const textoDestaque = {
+    color: '#00f',
+    fontSize: '3em'
+  }
+
   return (
     <>
-      <Header />
-      <Corpo />
-      <Footer copy='&copy' date={getDate()} direitos='sua Empresa. Todos os direitos reservados.' />
+     <section className="caixa">
+       <h1 style={{color: '#f00', fontSize: '5em'}}>CFB Cursos</h1>
+       <h2 style={textoDestaque}>Curso de React</h2>
+       <p className="texto">Se inscreva em nosso canal e nos siga no instagram</p>
+       <a href="https://www.youtube.com/cfbcursos" target="_blank">CFB cursos</a>
+     </section>
     </>
   )
 }
