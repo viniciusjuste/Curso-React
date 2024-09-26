a0c217bc48c64a30c8974a168b24e040bb052cd1
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-09-25 21:25:36 -0300

    Como funciona a renderização de componentes em React

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css' // É assim que se importa o arquivo CSS
+import Relogio from "./componentes/Relogio";
 import Header from "./componentes/Header";
 import Corpo from "./componentes/Corpo";
 import Footer, { getDate } from "./componentes/Footer"; // Importei o Footer e uma função que retorna o ano atual
@@ -13,12 +14,13 @@ export default function App() {
 
   return (
     <>
-     <section className="caixa">
-       <h1 style={{color: '#f00', fontSize: '5em'}}>CFB Cursos</h1>
-       <h2 style={textoDestaque}>Curso de React</h2>
-       <p className="texto">Se inscreva em nosso canal e nos siga no instagram</p>
-       <a href="https://www.youtube.com/cfbcursos" target="_blank">CFB cursos</a>
-     </section>
+      <section className="caixa">
+        <Relogio />
+        <h1 style={{ color: '#f00', fontSize: '5em' }}>CFB Cursos</h1>
+        <h2 style={textoDestaque}>Curso de React</h2>
+        <p className="texto">Se inscreva em nosso canal e nos siga no instagram</p>
+        <a href="https://www.youtube.com/cfbcursos" target="_blank">CFB cursos</a>
+      </section>
     </>
   )
 }
