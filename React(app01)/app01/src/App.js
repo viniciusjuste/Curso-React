93caaa10e81002b30e7bb203439e53338402f09e
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-10-01 20:31:59 -0300

    Eventos em React

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { useState } from "react"; // Tem que importar para usar State
 import './App.css' // É assim que se importa o arquivo CSS
-import Numero from "./componentes/Numero";
-import Relogio from "./componentes/Relogio";
-import Header from "./componentes/Header";
-import Corpo from "./componentes/Corpo";
 import Footer, { getDate } from "./componentes/Footer"; // Importei o Footer e uma função que retorna o ano atual
 
+import Led from './componentes/Led'
+
 export default function App() {
-  const [num, setNum] = useState(10) // State
-  const [nome, setNome] = useState('Sem nome');
+
+  const [Ligado, setLigado] = useState(false)
+
+  const cancelar = (obj) => {
+    return obj.preventDefault();
+  }
 
   return (
     <>
-      <p>Valor do state num em App: {num}</p>
-      <p>Nome: {nome}</p>
-      <Numero num={num} setNum={setNum} setNome={setNome} nome={nome} />
+      <Led ligado={Ligado} setLigado={setLigado} />
+      <a href="http://www.youtube.com/cfbcursos" target="_blank" onClick={(e) => cancelar(e)}>CFB Cursos</a>
     </>
   )
 }
