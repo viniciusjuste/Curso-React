11ef00bb028f090deca5ad529d81e14bd447a338
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-10-03 18:33:56 -0300

    Trabalhando com LISTA em React usando a função MAP

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -7,35 +7,15 @@ import Led from './componentes/Led'
 
 export default function App() {
 
-  const vermelho = { color: '#f00' };
-  const verde = { color: '#0f0' };
-  const azul = { color: '#00f' };
-
-  const [cor, setCor] = useState(1);
-
-  const retCor = (c) => {
-    if (c == 1) {
-      return vermelho
-    } else if (cor == 2) {
-      return verde
-    } else {
-      return azul
-    }
-  }
-
-  const mudaCor = () => {
-    setCor(cor + 1)
-   if(cor > 2){
-    setCor(1)
-   }
-  }
-
-  setInterval(mudaCor, 1000);
+  const carros = ['HRV', 'Golf', 'Focus', 'Cruze', 'Argo'];
+  const listaCarros = carros.map(
+    (c, i) => 
+    <li key={i}>{i}-{c}</li>
+  )
 
   return (
     <>
-      <h1 style={retCor(cor)}>CFB Cursos</h1>
-      <button onClick={() => mudaCor()}>Mudar cor</button>
+     <ul>{listaCarros}</ul>
     </>
   )
 }
