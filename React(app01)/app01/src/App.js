f51e9cdaad61d5bbbcddb04251db4e9ba9abca8a
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-10-09 19:55:01 -0300

    Aprendendo sobre o Hook UseEffect

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react"; // Tem que importar para usar State
 import './App.css' // É assim que se importa o arquivo CSS
 import Footer, { getDate } from "./componentes/Footer"; // Importei o Footer e uma função que retorna o ano atual
@@ -11,12 +11,19 @@ import Canal from "./componentes/Canal";
 
 export default function App() {
 
+  const [contagem, setContagem] = useState(0)
+
+  useEffect(
+    () => {
+      console.log('Página carregada');
+      document.title = 'Contagem: ' + contagem;
+    }
+  )
+
   return (
     <>
-     <Caixa site='www.cfbcursos.com.br'>
-      <Canal/>
-      <p>Curso de React</p>
-     </Caixa>
+      <p>Contagem: {contagem}</p>
+      <button onClick={() => setContagem(contagem + 1)}>Contar</button>
     </>
   )
 }
