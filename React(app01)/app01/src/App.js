22844b364db83db253fbca3398d1b946bcce6eb6
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-11-05 20:59:45 -0300

    Navegando entre telas de forma nativa em React

--- React(app01)/app01/src/App.js
+++ React(app01)/app01/src/App.js
@@ -5,29 +5,41 @@ import Carro from "./componentes/Carro";
 import BaseClasse from "./componentes/BaseClasse";
 import Globais from "./componentes/Globais";
 import Info from "./componentes/Info";
+import Pagina1 from "./componentes/Pagina 1";
+import Pagina2 from "./componentes/Pagina 2";
 
 export default function App() {
 
-  const [resumo, setResumo] = useState(Globais.resumo);
+  const [pagina, setPagina] = useState(0);
 
-  const gravarResumo = () => {
-    Globais.resumo = resumo;
+  useEffect(() => {
+    const url = window.location.href;
+    const urlSplit = url.split('?');
+    setPagina(urlSplit[1]);
+  })
+
+  const retornarPagina = () => {
+    if (pagina == 1) {
+      return <Pagina1 />;
+    } else if (pagina == 2) {
+      return <Pagina2 />;
+    } else {
+      return (
+        <div>
+          <button onClick={() => linksPagina(1)}>Pagina 1</button>
+          <button onClick={() => linksPagina(2)}>Pagina 2</button>
+        </div>
+      )
+    }
   }
 
-  const verResumo = () => {
-    alert(Globais.resumo);
+  const linksPagina = (number) => {
+    return window.open(`http://localhost:3000?${number}`, `_self`)
   }
 
   return (
     <>
-     <Info/>
-     <p>{'Canal ' + Globais.canal}</p>
-     <p>{'Curso ' + Globais.curso}</p>
-     <p>{'Ano ' + Globais.ano}</p>
-     <input type="text" value={resumo} onChange={(e) => setResumo(e.target.value)} />
-     <br/>
-     <button onClick={() => verResumo()}>Ver Resumo</button>
-     <button onClick={() => gravarResumo()}>Gravar Resumo</button>
+      {retornarPagina()}
     </>
   )
 
---
+++ React(app01)/app01/src/componentes/Pagina 2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Pagina2() {
+    return (
+        <>
+            <div>
+                <h1>Pagina 2</h1>
+                <h3>CFB Cursos</h3>
+            </div>
+        </>
+    )
+}
